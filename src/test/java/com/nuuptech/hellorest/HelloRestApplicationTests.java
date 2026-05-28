bd5ae8dd38a8832064b964d54c29c3f3c728929f
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2026-05-27 22:48:09 -0600

    Update: HelloRestApplicationTest

--- src/test/java/com/nuuptech/hellorest/HelloRestApplicationTests.java
+++ src/test/java/com/nuuptech/hellorest/HelloRestApplicationTests.java
@@ -5,21 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+class HelloRestApplicationTests {
 
-class SaludoApplicationTests {
- 	@Test
- 	void testPass() {
-     	assertEquals(2, 2); //paso para aprobar test
- }
+	@Test
+	void contextLoads() {
+	}
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-
-// @SpringBootTest
-// class HelloRestApplicationTests {
-
-// 	@Test
-// 	void contextLoads() {
-// 	}
-
-// }
+}
